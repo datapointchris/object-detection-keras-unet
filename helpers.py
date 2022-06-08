64fc02aec1f0e5527b7856177e8d45aa119c6f0d
Chris Birch <datapointchris@gmail.com>
2022-06-07 22:26:13 -0500

    refactor: Move batch processing to helpers

--- helpers.py
+++ helpers.py
@@ -2,11 +2,70 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from itertools import islice
+from functools import partial
+import tensorflow as tf
+import random
+from pathlib import Path
 
 
-def image_splitter(image, num_col_splits, num_row_splits):
+def load_dataset_path(path: Path, format='.tif', limit=None):
+    paths = sorted(list(path.glob(f'*{format}')))
+    return paths[:limit] if limit else paths
+
+
+def create_batches(iterable, batch_size):
+    iterator = iter(iterable)
+    while batch := list(islice(iterator, batch_size)):
+        yield batch
+
+
+def load_image(path, gray=False):
+    if gray:
+        return np.array(cv2.imread(str(path), cv2.IMREAD_GRAYSCALE), dtype=np.uint8)
+    else:
+        return np.array(cv2.imread(str(path)), dtype=np.uint8)
+
+
+def resize_image(image, resize_width, resize_height):
+    return cv2.resize(image, (resize_width, resize_height))
+
+
+def generate_augmentations(seed=77) -> dict:
+    seed = (seed, 0)  # to make tf seed happy
+    transforms = {
+        'brightness': partial(tf.image.stateless_random_brightness, max_delta=0.4, seed=seed),
+        'contrast': partial(tf.image.stateless_random_contrast, lower=0.1, upper=0.5, seed=seed),
+        'flip_horizontal': partial(tf.image.stateless_random_flip_left_right, seed=seed),
+        'flip_vertical': partial(tf.image.stateless_random_flip_up_down, seed=seed),
+        'hue': partial(tf.image.stateless_random_hue, max_delta=0.5, seed=seed),
+        'saturation': partial(
+            tf.image.stateless_random_saturation, lower=0.1, upper=0.5, seed=seed
+        ),
+    }
+    transform_range = range(0, len(transforms))
+    number_of_transforms = (random.choices(transform_range, weights=reversed(transform_range)))[0]
+    keys = random.sample(sorted(transforms), k=number_of_transforms)
+    print('AUGMENTATIONS RETURNED')
+    print(keys)
+    return {k: transforms[k] for k in keys}
+
+
+def augment_image(image, transforms, gray=False):
+    gray_keys = ['brightness', 'contrast', 'hue', 'saturation']
+    if gray:
+        transforms = {k: transforms[k] for k in transforms if k not in gray_keys}
+    if transforms:
+        new = image
+        for name in transforms:
+            transform = transforms.get(name)
+            new = transform(new)
+        return np.array(new)
+    return np.array(image)
+
+
+def split_image(image, col_splits, row_splits):
     """
-    Splits an image into 'num_col_splits' X 'num_row_splits'
+    Splits an image into 'col_splits' X 'row_splits'
     Returns array of images arranged from left -> right, top -> bottom
     """
 
@@ -14,13 +73,13 @@ def image_splitter(image, num_col_splits, num_row_splits):
     height = image.shape[1]
     imglist = []
 
-    for startpoint in np.linspace(0, width, num_col_splits, endpoint=False):
-        endpoint = startpoint + (width / num_col_splits)
+    for vstart in np.linspace(0, width, col_splits, endpoint=False):
+        vend = vstart + (width / col_splits)
 
-        for startp2 in np.linspace(0, height, num_row_splits, endpoint=False):
-            endp2 = startp2 + (height / num_row_splits)
+        for hstart in np.linspace(0, height, row_splits, endpoint=False):
+            hend = hstart + (height / row_splits)
 
-            imglist.append(image[int(startp2) : int(endp2), int(startpoint) : int(endpoint)])
+            imglist.append(image[int(hstart) : int(hend), int(vstart) : int(vend)])
     return np.array(imglist)
 
 
@@ -34,28 +93,42 @@ def print_array_properties(name, array):
     print('')
 
 
-def create_batches(iterable, batch_size):
-    iterator = iter(iterable)
-    while batch := list(islice(iterator, batch_size)):
-        yield batch
-
-
-def batch_stacker(batches, gray=False, resize: tuple[int] | None = None):
+def generate_training_batches(
+    path,
+    batch_size,
+    resize: tuple[int] | None = None,
+    split: tuple[int] | None = None,
+    augment: bool = None,
+    gray: bool = False,
+    seed: int = 77,
+    sync: list | None = None,
+):
+    batches = create_batches(load_dataset_path(path, limit=None), batch_size=batch_size)
     for batch in batches:
+        batch = (load_image(image) for image in batch)
         if gray:
-            arr = [
-                np.array(cv2.imread(image, cv2.IMREAD_GRAYSCALE), dtype=np.uint8)
-                for image in batch
-            ]
-            if resize:
-                arr = [cv2.resize(a, (resize[0], resize[1])) for a in arr]
-            arr = np.expand_dims(arr, axis=3)
-        else:
-            arr = [np.array(cv2.imread(image), dtype=np.uint8) for image in batch]
-            if resize:
-                arr = [cv2.resize(a, (resize[0], resize[1])) for a in arr]
-        stack = (np.stack(arr) / 255).astype(np.float32)
-        yield stack
+            batch = (
+                np.expand_dims(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), axis=2) for image in batch
+            )
+        if resize:
+            batch = (resize_image(image, resize[0], resize[1]) for image in batch)
+            if gray:
+                batch = (np.expand_dims(image, axis=2) for image in batch)
+        if augment:
+            print('INFO: Augmenting')
+            if sync:
+                batch = (augment_image(image, transforms=sync, gray=gray) for image in batch)
+            else:
+                transforms = generate_augmentations(seed=seed)
+                batch = (augment_image(image, transforms=transforms, gray=gray) for image in batch)
+        if split:
+            batch = (split_image(image, split[0], split[1]) for image in batch)
+        batch = np.stack(list(batch)) / 255
+        batch = batch.astype(np.float32)
+        if len(batch[0].shape) < 4:
+            batch = np.expand_dims(batch, axis=4)
+        batch = batch.reshape(-1, batch[0].shape[1], batch[0].shape[2], batch[0].shape[3])
+        yield batch
 
 
 def image_tester(original):
--- train.py
+++ train.py
@@ -1,74 +1,44 @@
-import random
-from pathlib import Path
-
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from keras.layers import Input
 from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 
 import config
 from callbacks import ValPlotCallback
 from helpers import (
-    batch_stacker,
-    create_batches,
+    augment_image,
+    generate_augmentations,
+    generate_training_batches,
     print_array_properties,
 )
 from model import make_model
 
 
-def load_image_datasets(images_path: Path, masks_path: Path):
-    train_image_path = images_path
-    mask_image_path = masks_path
-
-    filenames = [f.name for f in train_image_path.glob('*.tif')]
-    random.shuffle(filenames)
-
-    train_files = [str(train_image_path / name) for name in filenames]
-    mask_files = [str(mask_image_path / name) for name in filenames]
-
-    return train_files, mask_files
-
-
-def augment_images(x_train, y_train, datagen_params: config.DatagenParams, seed: int) -> tuple:
-    image_datagen = ImageDataGenerator(**datagen_params.as_dict())
-    mask_datagen = ImageDataGenerator(**datagen_params.as_dict())
-
-    image_datagen.fit(x_train, augment=True, seed=seed)
-    mask_datagen.fit(y_train, augment=True, seed=seed)
-
-    image_generator = image_datagen.flow(x_train, seed=seed)
-    mask_generator = mask_datagen.flow(y_train, seed=seed)
-
-    return zip(image_generator, mask_generator)
-
-
-def main_training_loop(loop_epochs=1, augment_data=False, show_validation_plots=False):
+def main_training_loop(loop_epochs, show_batch_data_shapes=True, show_validation_plots=False):
     for loop_epoch in range(1, loop_epochs + 1):
-        print(f'Training Loop Epoch: {loop_epoch} of {loop_epochs}')
-        if loop_epoch == 1:
-            augment_data = False  # do not augment the first loop of original images
+        print(f'LOOP EPOCH: {loop_epoch} of {loop_epochs}')
 
-        train_files, mask_files = load_image_datasets(
-            images_path=config.main.train_image_path, masks_path=config.main.train_mask_path
-        )
-
-        train_img_batches = create_batches(train_files, batch_size=config.model_params.batch_size)
-        train_mask_batches = create_batches(mask_files, batch_size=config.model_params.batch_size)
-
-        train_stack = batch_stacker(
-            train_img_batches, resize=(config.image.resize_width, config.image.resize_height)
-        )
-        mask_stack = batch_stacker(
-            train_mask_batches,
+        batch_params = dict(
+            batch_size=config.image.image_batch_size,
             resize=(config.image.resize_width, config.image.resize_height),
-            gray=True,
+            split=(config.image.split_cols, config.image.split_rows),
+            augment=config.image.augment,
+            seed=config.main.SEED,
+        )
+        augment_params = generate_augmentations(seed=config.main.SEED)
+        train_batches = generate_training_batches(
+            path=config.main.train_image_path, gray=False, sync=augment_params, **batch_params
+        )
+        mask_batches = generate_training_batches(
+            path=config.main.train_mask_path, gray=True, sync=augment_params, **batch_params
         )
 
         for batch_number, (train_batch, mask_batch) in enumerate(
-            zip(train_stack, mask_stack), start=1
+            zip(train_batches, mask_batches), start=1
         ):
             print(f'BATCH NUMBER {batch_number}')
+            print('AUGMENT PARAMS')
+            print(augment_params.keys())
 
             x_train, x_val, y_train, y_val = train_test_split(
                 train_batch,
@@ -76,21 +46,15 @@ def main_training_loop(loop_epochs=1, augment_data=False, show_validation_plots=
                 random_state=config.main.SEED,
                 test_size=config.model.train_val_split_size,
             )
-            if batch_number == 1:
+            if batch_number == 1 and show_batch_data_shapes:
                 print_array_properties('x_train', x_train)
                 print_array_properties('y_train', y_train)
                 print_array_properties('x_val', x_val)
                 print_array_properties('y_val', y_val)
 
-            if augment_data:
-                print('Using Data Augmentation')
-                x_train, y_train = augment_data(
-                    x_train, y_train, datagen_params=config.datagen_params, seed=config.main.SEED
-                )
-
             inputs = Input(shape=(x_train.shape[1:]))
 
-            if config.model.pretrained or batch_number > 1:
+            if config.model.pretrained or batch_number > 1 or loop_epoch > 1:
                 print('Loading Trained Model')
                 model = load_model(config.model.full_path)
             else:
@@ -110,9 +74,11 @@ def main_training_loop(loop_epochs=1, augment_data=False, show_validation_plots=
             validation_plots = ValPlotCallback(
                 model=model, batch_size=config.model_params.batch_size, x_val=x_val, y_val=y_val
             )
+
             all_callbacks = [early_stop, check_point, tensor_board]
             if show_validation_plots:
                 all_callbacks.append(validation_plots)
+
             model_fit_params = dict(
                 validation_data=(x_val, y_val),
                 steps_per_epoch=max(x_train.shape[0] // config.model_params.batch_size, 1),
